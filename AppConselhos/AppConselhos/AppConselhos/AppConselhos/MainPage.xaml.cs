ab7d1001a49b382150136e3a91ea7f4562ad975b
Enzo Vendramini Paggiaro <e2enzo2@gmail.com>
2022-11-28 12:59:11 -0300

    Add files via upload

---
+++ AppConselhos/AppConselhos/AppConselhos/AppConselhos/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppConselhos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+       var result = DisplayAlert("Alerta", "Deseja realmente sair?", "ok");
+    } 
+
+    }
+}
